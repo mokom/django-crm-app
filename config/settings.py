a39463a9d13496c17bc36742ddae0724d51c45e5
mokom <mokomgeh45@gmail.com>
2020-03-04 14:33:03 +0100

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -1,4 +1,5 @@
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -154,3 +155,5 @@ EMAIL_HOST_PASSWORD = 'publishingstore.com'
 # AWS_DEFAULT_ACL = None
 # DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 # STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+django_heroku.settings(locals())
